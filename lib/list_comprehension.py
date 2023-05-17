19e8563fc9f84dc646d21a4d0762f53d607e6229
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-17 17:06:35 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 == 0]
+   
+  
+    # pass
 
 def make_exclamation(sentence_list):
+    return [ x + "!"  for x in sentence_list]
     pass
